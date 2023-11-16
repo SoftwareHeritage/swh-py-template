85f57c925f67867fd852749835f49acf5091b687
David Douard <david.douard@sdfa3.org>
2023-11-16 10:24:44 +0100

    template: remove the ensure_readme hook from the docs' sphinx config
    This is handled in swh-docs directly.

--- copier-template/docs/conf.py
+++ copier-template/docs/conf.py
@@ -1,19 +1,1 @@
-from pathlib import Path
-
 from swh.docs.sphinx.conf import *  # NoQA
-
-
-def ensure_readme(app, config):
-    srcpath = Path(app.srcdir)
-
-    for extension in ("rst", "md"):
-        fname = f"README.{extension}"
-        readme_file = srcpath / ".." / fname
-        symlink = srcpath / fname
-        if readme_file.exists() and not symlink.exists():
-            symlink.symlink_to(readme_file)
-            break
-
-
-def setup(app):
-    app.connect("config-inited", ensure_readme)
